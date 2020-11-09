652880c4520d1f4417ecd91fd7585076c122c213
wyupeng <wangyupeng_ly@163.com>
2020-11-09 17:01:04 +0800

    装饰模式

--- src/main/java/com/wyp/decorator/Client.java
+++ src/main/java/com/wyp/decorator/Client.java
@@ -1,5 +1,14 @@
 package com.wyp.decorator;
 
+/**
+ * 装饰模式
+ * 优点：
+ * 1.不改变原有对象的情况下给一个对象扩展功能；
+ * 2.使用不同的组合可以实现不同的效果；
+ * 3.符合开闭原则
+ * 缺点：
+ * 1.多层的装饰会使系统比较复杂。即一个实现类的功能若用多个装饰类进行装饰，则会增加该实现类的耦合度。
+ */
 public class Client {
     public static void main(String[] args) {
         // 添加美颜
